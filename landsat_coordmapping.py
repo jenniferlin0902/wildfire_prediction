81992fa849748aa0b52ddb185854374308f9a646
Yanbing Zhu <yanbingzhu@DN51s43t.SUNet>
2018-02-23 00:14:54 -0800

    fixed cropping

--- landsat_coordmapping.py
+++ landsat_coordmapping.py
@@ -8,6 +8,8 @@ from pprint import pprint
 FIRE_LABEL_FILE='data/landsat_fire_last7.csv'
 DEFAULT_DOWNLOAD_PATH='landsat_download'
 
+# not including IR data right now
+
 # sorry, hardcoded & hopefully only called once (angle = 14) 
 def find_rotation_angle(data):
     x_values = data[:,0,0]
@@ -33,7 +35,7 @@ def find_rotation_angle(data):
                 break
         if (xpad != -1):
             break
-    #got results: value: 244 index: 14, 1583
+    # got results: value: 244 index: 14, 1583
     # second value: 20 index: 6351, 140; so 1583/6351
 
 def straighten_image(imagename):
@@ -69,37 +71,44 @@ def find_corner_coords(filename):
 def map_fires(image, coords, filename, label, mini_size):
     data = np.asarray(image)
     num_across = min(data.shape[0], data.shape[1])/mini_size
-    is_fire = np.zeros((num_across, num_across))
+    is_fire = np.zeros((num_across * num_across))
 
     with open(filename) as f:
         data = json.load(f)
 
-    for i in range(len(data[label])):
-        print data[label]['lat'][i]
-        lat = data[label]['lat'][i]
-        lon = data[label][i]['lon'][i]
-        lat_index = (lat - coords['ll_lat'])/mini_size
-        lon_index = (lon - coords['ul_lon'])/mini_size
+    for i in range(len(data[unicode(label)])):
+   
+        lat = data[label]['lats'][i]
+        lon = data[label]['lons'][i]
+
+        lat_range = abs(coords['ul_lat'] - coords['ll_lat'])
+        lon_range = abs(coords['ur_lon'] - coords['ul_lon'])
+
+
+        lat_index = int((lat - coords['ll_lat'])/(lat_range/num_across))
+        lon_index = int((lon - coords['ul_lon'])/(lon_range/num_across))
+
 
         if (lat_index < num_across and lon_index < num_across):
-            is_fire[lat_index*num_across + lon_index] = 1
+            is_fire[lon_index*num_across + lat_index] = 1
+            print str(lon_index*num_across + lat_index)
+
     return is_fire
 
 def image_blocks(image, label, is_fire, mini_size):
     data = np.asarray(image)
-    mini_size = is_fire.shape[0]
     size = (mini_size, mini_size)
 
     # number of mini images that can fit in one dimension
     num_across = min(data.shape[0], data.shape[1])/mini_size
 
+    # crop dimensions (left, upper, right, lower) 
     for i in range(num_across):
         for j in range(num_across):
-            temp = image.crop((i * mini_size, (i+1) * mini_size, j * mini_size, (j+1) * mini_size))
-            #fire_label = get_fire_label()
-            # TODO is this use ????
+            temp = image.crop((i * mini_size, j * mini_size, (i+1) * mini_size, (j+1) * mini_size))
             # fire_label: 0 for no fire, 1 for fire
-            outfile_name = label + '_' + str(i*num_across + j) + is_fire[i*num_across + j] + '.jpg'
+            outfile_name = label + '_' + str(i*num_across + j) + '_' + str(int(is_fire[i*num_across + j])) + '.jpg'
+            print outfile_name
             temp.save(outfile_name, "JPEG")
 
 if __name__ == '__main__':
@@ -111,7 +120,8 @@ if __name__ == '__main__':
     mini_size = 300
 
     for label in image_list:
-        label = 'LC08_L1TP_026032_20180211_20180211_01_RT'
+        # remove the label
+        label = 'LC08_L1TP_027033_20180218_20180218_01_RT'
         image_dir = os.path.join(images_base, label)
         irimage = os.path.join(image_dir, label + '_B7.TIF')
         processedimage = os.path.join(os.path.join(image_dir, label), label + '_bands_234.TIF')
@@ -123,5 +133,7 @@ if __name__ == '__main__':
         # returns an array: index corresponds to label of mini image
         # 0 for no fire, 1 for fire
         is_fire = map_fires(image, coords, firefile, label, mini_size)
+        image_blocks(image, label, is_fire, mini_size)
 
-        image_blocks(image, label, is_fire)
+        # remove the break
+        break
