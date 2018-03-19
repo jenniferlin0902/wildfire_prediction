dae9e717a2e41ebeaf2f6a08c25250b4b74c4de1
Jennifer <jennlinc@stanford.edu>
2018-03-18 22:36:42 -0700

    Update evaluate.py to work with fire diata

--- vision/evaluate.py
+++ vision/evaluate.py
@@ -10,13 +10,13 @@ from model.input_fn import input_fn
 from model.model_fn import model_fn
 from model.evaluation import evaluate
 from model.utils import Params
-from model.utils import set_logger
+from model.utils import set_logger, is_fire
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', default='experiments/test',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+parser.add_argument('--data_dir', default='data/',
                     help="Directory containing the dataset")
 parser.add_argument('--restore_from', default='best_weights',
                     help="Subdirectory of model dir or file containing the weights")
@@ -38,13 +38,13 @@ if __name__ == '__main__':
     # Create the input data pipeline
     logging.info("Creating the dataset...")
     data_dir = args.data_dir
-    test_data_dir = os.path.join(data_dir, "test_signs")
+    test_data_dir = os.path.join(data_dir, "test_images")
 
     # Get the filenames from the test set
     test_filenames = os.listdir(test_data_dir)
     test_filenames = [os.path.join(test_data_dir, f) for f in test_filenames if f.endswith('.jpg')]
 
-    test_labels = [int(f.split('/')[-1][0]) for f in test_filenames]
+    test_labels = [is_fire(os.path.basename(f)) for f in test_filenames]
 
     # specify the size of the evaluation set
     params.eval_size = len(test_filenames)
