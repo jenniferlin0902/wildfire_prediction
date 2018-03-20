9ede0f00f1538891b35346b65c7762abccaf0185
Jennifer Lin <clin3@caltech.edu>
2018-03-20 07:03:21 +0000

    Update evaluation to work with text summary

--- vision/model/evaluation.py
+++ vision/model/evaluation.py
@@ -28,22 +28,22 @@ def evaluate_sess(sess, model_spec, num_steps, writer=None, params=None):
     sess.run(model_spec['metrics_init_op'])
 
     # compute metrics over the dataset
-    for _ in range(num_steps):
-        sess.run(update_metrics)
-
+    for s in range(num_steps):
+        _, summ  = sess.run([update_metrics, model_spec["summary_op"]])
+        writer.add_summary(summ, s)
     # Get the values of the metrics
     metrics_values = {k: v[0] for k, v in eval_metrics.items()}
-    metrics_val = sess.run(metrics_values)
+    metrics_val, summ  = sess.run([metrics_values, model_spec['summary_op']])
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_val.items())
     logging.info("- Eval metrics: " + metrics_string)
 
     # Add summaries manually to writer at global_step_val
-    if writer is not None:
-        global_step_val = sess.run(global_step)
-        for tag, val in metrics_val.items():
+    #if writer is not None:
+ #       global_step_val = sess.run(global_step)
+#        #for tag, val in metrics_val.items():dd
             #summ = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=val)])
-            summ = sess.run(model_spec['summary_op'])
-            writer.add_summary(summ, global_step_val)
+         #   summ = sess.run(model_spec['summary_op'])
+    #    writer.add_summary(summ, global_step_val)
 
     return metrics_val
 
